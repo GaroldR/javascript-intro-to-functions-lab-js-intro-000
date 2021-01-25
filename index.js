c13f763ee800982c6074e72e19f428e84060aaa8
Garold Rabinovich <garold.rabinovich@gmail.com>
2021-01-25 05:34:49 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) { return string.toUpperCase() }
+function whisper(string) { return string.toLowerCase() }
+function logShout(string) {console.log(string.toUpperCase());}
+function logWhisper(string) {console.log(string.toLowerCase());}
+
+SayHighToGrandma(string) {
+var cantHear = "I can't hear you!";
+var hearYou = "YES INDEED!";
+var isLove = "I love you, too.";
+
+
+ if (string.toLowerCase())
+{
+  return cantHear;
+}
+else if (string.toUpperCase())
+{
+  return hearYou;
+}
+  else if ("I love you, Grandma.")
+  {
+    return isLove;
+  }
+}
